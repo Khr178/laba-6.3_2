24d024e66882c660de0d44ee65dd8e18377ff59c
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-17 15:39:41 +0200

    first master commit

---
+++ лаба 6.3.2.cpp
@@ -0,0 +1,46 @@
+﻿// лаба 6.3.2.cpp 
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create1(int* b, const int size, const int Low, const int High, int i)
+{
+	b[i] = Low + rand() % (High - Low + 1);
+	if (i < size - 1)
+		Create1(b, size, Low, High, i + 1);
+}
+
+
+void Print1(int* b, const int size, int i)
+{
+	cout << setw(4) << b[i];
+	if (i < size - 1)
+		Print1(b, size, i + 1);
+	else
+		cout << endl;
+}
+
+int Sum1(int* b, const int size, int i, int S)
+{
+	if (b[i] < 0)
+		S += b[i];
+	if (i < size - 1)
+		return Sum1(b, size, i + 1, S);
+	else
+		return S;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 24;
+	int b[n];
+	int Low = -15;
+	int High = 75;
+	Create1(b, n, Low, High, 0);
+	Print1(b, n, 0);
+	cout << "s = " << Sum1(b, n, 0, 0) << endl;
+	return 0;
+}
---
+++ юніт тест 6.3.2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 6.3.2/лаба 6.3.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест632
+{
+	TEST_CLASS(юніттест632)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s[1] = { 1 };
+			int b;
+			b = Sum1(s, 0, 0, 0);
+			Assert::AreEqual(b, 0);
+		}
+	};
+}
